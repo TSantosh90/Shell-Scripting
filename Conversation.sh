bd587a4cf92e56b1d51c6bd48411517e4447d05b
Santosh kumar <santosh30901@gmail.com>
2026-02-01 10:16:42 +0530

    actions

---
+++ Conversation.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+echo "Ramesh:: How are Suresh"
+echo "Suresh :: Im doing good what about you Ramesh"
+echo "Ramesh :: Im good what are you learing"
+echo "Suresh :: Im learning linux basics"
