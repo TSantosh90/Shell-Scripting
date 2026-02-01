bc440bc6a5b1a67170c9088c0e828a0495821bca
Santosh kumar <santosh30901@gmail.com>
2026-02-01 10:33:48 +0530

    act

---
+++ Variables-1.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: How are $PERSON2"
+echo "$PERSON2 :: Im doing good what about you $PERSON1"
+echo "$PERSON1 :: Im good what are you learing"
+echo "$PERSON2 :: Im learning linux basics"
