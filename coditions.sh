c668c1336097b723b0e0ce24634b1eda69c4fd03
Santosh kumar <santosh30901@gmail.com>
2026-02-01 16:10:53 +0530

    act

--- coditions.sh
+++ coditions.sh
@@ -3,5 +3,5 @@ NUMBER=$1
 if [ $NUMBER -gt 20 ]; then
     echo "Given number : $NUMBER is greater than 20"
 else
-    echo "Given number : $UMBER is less than 20"
+    echo "Given number : $NUMBER is less than 20"
 fi
