12fb33843115ee86573b9fbbca622645a0a23470
Santosh kumar <santosh30901@gmail.com>
2026-02-07 08:07:43 +0530

    logs

---
+++ 17-trap.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e 
+trap 'echo "There is an error in $LINENO, Command: $BASH_COMMAND"' ERR
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+    echo-e "$R please run this with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE () { ##function
+  if [ $1 -ne 0 ]; then
+     echo -e "$2.....$R Failure $N" | tee -a $LOGS_FILE
+     exit 1
+  else 
+     echo -e "$2.... $Y Success $N" | tee -a $LOGS_FILE
+
+    fi  
+}
+
+for package in $@ # sudo 12-loops.sh nginx mysql nodejs  #loops concept
+do
+  dnf list installed $package -y &>>LOGS_FILE
+    if [ $? -ne 0 ];then
+      echo -e " $Y $package not installed,installing now $N"
+      dnf install $package -y &>>$LOGS_FILE
+      VALIDATE $? "$package installation"
+    else
+      echo -e " $Y $package already installed,skip for now $N"
+    fi
+done
