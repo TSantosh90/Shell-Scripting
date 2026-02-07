c83d29d59fe5342c6dd4cfb627dec86872c9e64a
Santosh kumar <santosh30901@gmail.com>
2026-02-07 07:40:34 +0530

    logs

---
+++ 14-colours.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m"
+
+echo -e "$R hello world $N , $Y im learning linux $N"
+
+echo -e "$B let me know your doubts $N , I am able to answer $N"
+
