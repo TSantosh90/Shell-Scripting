0e7f6bd3341c4a864b17d48d6cacf6f98723a870
Santosh kumar <santosh30901@gmail.com>
2026-02-07 06:27:15 +0530

    logs

--- 10-logs.sh
+++ 10-logs.sh
@@ -5,7 +5,7 @@ LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILE="/var/log/shell-script/$0"
 
 if [ $USERID -ne 0 ]; then
-    echo "please run this with root user access"
+    echo "please run this with root user access" | tee -a $LOGS_FILE
     exit 1
 fi
 
@@ -13,10 +13,10 @@ mkdir -p $LOGS_FOLDER
 
 VALIDATE () { ##function
   if [ $1 -ne 0 ]; then
-     echo "$2..... Failure"
+     echo "$2..... Failure" | tee -a $LOGS_FILE
      exit 1
   else 
-     echo "$2.... Success"
+     echo "$2.... Success" | tee -a $LOGS_FILE
 
     fi  
 }
