b64d253e49fa35672f6bf9245e4626418b98b6c5
Santosh kumar <santosh30901@gmail.com>
2026-02-07 07:15:14 +0530

    logs

---
+++ 13-loops.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0"
+
+if [ $USERID -ne 0 ]; then
+    echo "please run this with root user access" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE () { ##function
+  if [ $1 -ne 0 ]; then
+     echo "$2..... Failure" | tee -a $LOGS_FILE
+     exit 1
+  else 
+     echo "$2.... Success" | tee -a $LOGS_FILE
+
+    fi  
+}
+
+for package in $@ # sudo 12-loops.sh nginx mysql nodejs  #loops concept
+do
+  dnf list package installed -y &>>LOGS_FILE
+    if [$? -ne 0];then
+      echo "$package not installed.installing now"
+      dnf install $package -y &>>$LOGS_FILE
+      VALIDATE $? "$package installation"
+    else
+      echo "package already installed.skip for now"
+    fi
+done
